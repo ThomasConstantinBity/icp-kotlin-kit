c8354a4ecb9d8b76879156df4ba3aeeb6c015682
Thomas Constantin <thomas.constantin@bity.com>
2024-07-05 23:43:20 +0200

    add CRC32 with tests

---
+++ icp_kotlin_kit/icp_cryptography/src/main/java/com/bity/icp_cryptography/model/error/CRC32.kt
@@ -0,0 +1,25 @@
+package com.bity.icp_cryptography.model.error
+
+import java.nio.ByteBuffer
+import java.util.zip.CRC32
+
+object CRC32 {
+
+    /**
+     * com.bity.icp_cryptography.model.error.CRC32 is a four byte check sequence, calculated as defined by ISO 3309, ITU-T V.42,
+     * and elsewhere, and stored as big-endian, i.e., the most significant byte comes first and
+     * then the less significant bytes come in descending order of significance (MSB B2 B1 LSB).
+     */
+    private const val CRC_32_LENGTH = 4
+
+    fun crc32(data: ByteArray): ByteArray {
+        val crc32 = CRC32()
+        crc32.update(data)
+        val checksum = crc32.value
+
+        val buffer = ByteBuffer.allocate(Long.SIZE_BYTES)
+        buffer.putLong(checksum)
+
+        return buffer.array().takeLast(CRC_32_LENGTH).toByteArray()
+    }
+}
---
+++ icp_kotlin_kit/icp_cryptography/src/test/kotlin/com/bity/icp_cryptography/CRC32Test.kt
@@ -0,0 +1,59 @@
+package com.bity.icp_cryptography
+
+import com.bity.icp_cryptography.model.error.CRC32
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+@OptIn(ExperimentalStdlibApi::class)
+class CRC32Test {
+
+    @ParameterizedTest
+    @MethodSource("crc32TestValues")
+    fun crc32(
+        data: ByteArray,
+        expectedValue: ByteArray
+    ) {
+        val crc32 = CRC32.crc32(data)
+        assertArrayEquals(expectedValue, crc32)
+    }
+
+    companion object {
+        @JvmStatic
+        private fun crc32TestValues() = listOf(
+            Arguments.of(
+                byteArrayOf(),
+                "00000000".hexToByteArray()
+            ),
+            Arguments.of(
+                "123456789".toByteArray(),
+                "cbf43926".hexToByteArray()
+            ),
+            Arguments.of(
+                "a".toByteArray(),
+                "e8b7be43".hexToByteArray()
+            ),
+            Arguments.of(
+                "abc".toByteArray(),
+                "352441c2".hexToByteArray()
+            ),
+            Arguments.of(
+                "message digest".toByteArray(),
+                "20159d7f".hexToByteArray()
+            ),
+            Arguments.of(
+                "abcdefghijklmnopqrstuvwxyz".toByteArray(),
+                "4c2750bd".hexToByteArray()
+            ),
+            Arguments.of(
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".toByteArray(),
+                "1fc2e6d2".hexToByteArray()
+            ),
+            Arguments.of(
+                "12345678901234567890123456789012345678901234567890123456789012345678901234567890".toByteArray(),
+                "7ca94a72".hexToByteArray()
+            )
+        )
+    }
+}
