dc7a958b1aee3675c920145a38262b7a97d6f683
Thomas Constantin <thomas.constantin@bity.com>
2024-07-05 23:43:04 +0200

    add test libs

--- icp_kotlin_kit/icp_cryptography/build.gradle.kts
+++ icp_kotlin_kit/icp_cryptography/build.gradle.kts
@@ -7,3 +7,14 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+
+dependencies {
+
+    testImplementation(libs.bundles.junit.test)
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
