03d35107edf3ed142a6bd432a4eeb105acf642d0
Thomas Constantin <thomas.constantin@bity.com>
2024-07-06 00:12:46 +0200

    add SHA256 with tests

---
+++ icp_kotlin_kit/icp_cryptography/src/main/java/com/bity/icp_cryptography/SHA256.kt
@@ -0,0 +1,15 @@
+package com.bity.icp_cryptography
+
+import java.security.MessageDigest
+
+object SHA256 {
+
+    private const val SHA_256_ALGORITHM_NAME = "SHA-256"
+
+    private val messageDigest = MessageDigest.getInstance(SHA_256_ALGORITHM_NAME)
+    fun sha256(data: ByteArray): ByteArray =
+        messageDigest.digest(data)
+
+    fun doubleSha256(data: ByteArray): ByteArray =
+        sha256(sha256(data))
+}
