a2d77c058476f78a94c2ac401aae7cf1cf8689ce
Thomas Constantin <thomas.constantin@bity.com>
2024-09-26 23:28:55 +0200

    remove unused import

--- demo_app/src/main/java/com/bity/demo_app/data/DataModule.kt
+++ demo_app/src/main/java/com/bity/demo_app/data/DataModule.kt
@@ -1,7 +1,6 @@
 package com.bity.demo_app.data
 
 import com.bity.icp_kotlin_kit.domain.model.enum.ICPSystemCanisters
-import com.bity.icp_kotlin_kit.domain.usecase.ICRC1
 import com.bity.icp_kotlin_kit.domain.usecase.LedgerCanister
 import com.bity.icp_kotlin_kit.domain.usecase.Tokens
 import org.koin.dsl.module
